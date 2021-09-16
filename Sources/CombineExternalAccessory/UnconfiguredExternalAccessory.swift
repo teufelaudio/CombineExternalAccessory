b784afe54092d5bc313507680262c808afce49e3
Giulia Ariu <giulia.ariu@teufel.de>
2021-09-16 16:34:21 +0200

    Combine all properties in hash function instead of accessory

--- Sources/CombineExternalAccessory/UnconfiguredExternalAccessory.swift
+++ Sources/CombineExternalAccessory/UnconfiguredExternalAccessory.swift
@@ -71,7 +71,11 @@ extension UnconfiguredExternalAccessory: Equatable {
 // MARK: - Hashable
 extension UnconfiguredExternalAccessory: Hashable {
     public func hash(into hasher: inout Hasher) {
-        hasher.combine(accessory)
+        hasher.combine(name)
+        hasher.combine(manufacturer)
+        hasher.combine(model)
+        hasher.combine(ssid)
+        hasher.combine(macAddress)
     }
 }
 
